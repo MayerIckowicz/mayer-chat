28c42271f527bef598903633bff0aa1193881913
MayerIckowicz <callejonmayer@hotmail.com>
2022-01-16 12:33:06 -0300

    refactored the databasereq file

--- js/databaseReq.js
+++ js/databaseReq.js
@@ -1,14 +1,21 @@
 import { fetchDatabaseMessages } from "./initChatRoom.js";
 import getUserTokenFromLocalStorage from "./loginDetails.js";
 
-const getMessagesDatabase = async () => {
+const databaseRequestConfig = () => {
   const { idToken: USER_IDTOKEN, email: USER_EMAIL } =
     getUserTokenFromLocalStorage();
 
-  const DATABASE_NAME = `https://mayer-chat-default-rtdb.europe-west1.firebasedatabase.app/.json?auth=${USER_IDTOKEN}`;
+  return [
+    `https://mayer-chat-default-rtdb.europe-west1.firebasedatabase.app/.json?auth=${USER_IDTOKEN}`,
+    USER_EMAIL,
+  ];
+};
+
+const getMessagesDatabase = async () => {
+  const [requestLink] = databaseRequestConfig();
 
   try {
-    const resp = await fetch(DATABASE_NAME, {
+    const resp = await fetch(requestLink, {
       method: "GET",
     });
     const data = await resp.json();
@@ -24,13 +31,11 @@ export const sendMessageToDatabase = async () => {
   const inputMessage = document.querySelector(".chat__sendmessage--inp");
   const message = inputMessage.value;
   inputMessage.value = "";
-  const { idToken: USER_IDTOKEN, email: USER_EMAIL } =
-    getUserTokenFromLocalStorage();
 
-  const DATABASE_NAME = `https://mayer-chat-default-rtdb.europe-west1.firebasedatabase.app/.json?auth=${USER_IDTOKEN}`;
+  const [requestLink, USER_EMAIL] = databaseRequestConfig();
 
   try {
-    const resp = await fetch(DATABASE_NAME, {
+    const resp = await fetch(requestLink, {
       method: "POST",
       body: JSON.stringify({
         room: localStorage.getItem("room"),
